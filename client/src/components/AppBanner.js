09e80bdd81a6424c579560f55052f0abd565f90e
Sebastian Valdez <sebastianvaldez831@gmail.com>
2023-10-02 12:59:10 -0400

    State Select Drop Down
    Changed the state drop down so it looks better B)

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -1,40 +1,49 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography"
-import IconButton from "@mui/material/IconButton"
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
 
-
-import RefreshIcon from '@mui/icons-material/Refresh';
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 import eaglesLogo from "./images/eagle.png";
 
 export default function AppBanner() {
+  // Placeholder, for now it refreshes the page but it should probably be that only the gui components revert
+  const handleRefreshButton = () => {
+    window.location.reload();
+  };
 
-    // Placeholder, for now it refreshes the page but it should probably be that only the gui components revert 
-    const handleRefreshButton = () => {
-        window.location.reload()
-    }
-
-    return (
-       <AppBar sx={{bgcolor: "#2f2f30"}} style={{height: "7vh"}} position="static">
-        <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
-            <Box>
-                <img src={eaglesLogo} height={60} width={60}/>
-            </Box>
-            <Typography variant="h4" fontWeight="bold">
-                Cluster Analysis
-            </Typography>
-            <Box>
-                <IconButton 
-                    size="medium" 
-                    sx={{bgcolor: "#544c4c", color: "white"}} 
-                    style={{ borderRadius: "15px", fontWeight:"bold"}}
-                    onClick={handleRefreshButton}>
-                    <RefreshIcon sx={{color: "white", marginRight: "3px"}}></RefreshIcon>
-                </IconButton>
-            </Box>
-        </Toolbar>
-       </AppBar>
-    );
+  return (
+    <AppBar
+      sx={{ bgcolor: "#2f2f30" }}
+      style={{ height: "7vh" }}
+      position="static"
+    >
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <img src={eaglesLogo} height={60} width={60} />
+        </Box>
+        <Typography variant="h4" fontWeight="bold">
+          Cluster Analysis
+        </Typography>
+        <Box>
+          <IconButton
+            size="medium"
+            sx={{ bgcolor: "#544c4c", color: "white" }}
+            style={{ borderRadius: "15px", fontWeight: "bold" }}
+            onClick={handleRefreshButton}
+          >
+            <RefreshIcon sx={{ color: "white" }}></RefreshIcon>
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- client/src/components/StateSelectBanner.js
+++ client/src/components/StateSelectBanner.js
@@ -1,8 +1,8 @@
-import { GlobalStoreContext } from '../store'
-import { useContext, useState } from 'react'
-import AZBorders from './geojson/AZBorders.json';
-import SCBorders from './geojson/SCBorders.json';
-import TXBorders from './geojson/TXBorders.json';
+import { GlobalStoreContext } from "../store";
+import { useContext, useState } from "react";
+import AZBorders from "./geojson/AZBorders.json";
+import SCBorders from "./geojson/SCBorders.json";
+import TXBorders from "./geojson/TXBorders.json";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Select from "@mui/material/Select";
@@ -12,42 +12,73 @@ import InputLabel from "@mui/material/InputLabel";
 import Button from "@mui/material/Button";
 
 export default function StateSelectBanner() {
-    const { store } = useContext(GlobalStoreContext);
-    let curState = store.currentState !== "" ? store.currentState : ""
-    console.log("CUR STATE " + curState)
-    if (curState !== "") {
-        console.log(curState.features[0].properties.NAME)
-    }
-    let dropLabel = curState !== "" ? curState.features[0].properties.NAME : "Select State"
+  const { store } = useContext(GlobalStoreContext);
+  let curState = store.currentState !== "" ? store.currentState : "";
+  console.log("CUR STATE " + curState);
+  if (curState !== "") {
+    console.log(curState.features[0].properties.NAME);
+  }
 
-    const handleSelectAZ = () => {
-        store.setState(AZBorders)
-    }
-    const handleSelectTX = () => {
-        store.setState(TXBorders)
-    }
-    const handleSelectSC = () => {
-        store.setState(SCBorders)
-    }
+  let dropLabel =
+    curState !== "" ? curState.features[0].properties.NAME : "Select State";
+  console.log(`Drop Label: ${dropLabel}`);
 
-    return (
-        <Grid container spacing={0} sx={{bgcolor: "white"}} style={{height: "8vh", display: "flex", alignItems: "center"}}>
-            <Grid item xs={4}>
-                <FormControl style={{width: "13vw"}}>
-                    <InputLabel htmlFor="this_is_a_label" sx={{bgcolor: "white"}} style={{fontWeight: "bold"}}>{dropLabel}</InputLabel>
-                    <Select labelId="this_is_a_label">
-                        <MenuItem onClick={handleSelectAZ}>Arizona</MenuItem>
-                        <MenuItem onClick={handleSelectSC}>South Carolina</MenuItem>
-                        <MenuItem onClick={handleSelectTX}>Texas</MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
-            <Grid item xs={4}>
-                <Button variant="contained" style={{height: "5.5vh", width: "13vw", fontWeight: "bold"}}>This Is A Button</Button>
-            </Grid>
-            <Grid item xs={4}>
-                <Button variant="contained" style={{height: "5.5vh", width: "13vw", fontWeight: "bold"}}>This Is A Button 2</Button>
-            </Grid>
-        </Grid>
-    )
+  const handleChange = (event) => {
+    let stateValue = event.target.value;
+
+    if (stateValue == "Arizona") {
+      store.setState(AZBorders);
+    } else if (stateValue == "South Carolina") {
+      store.setState(SCBorders);
+    } else if (stateValue == "Texas") {
+      store.setState(TXBorders);
+    }
+  };
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      sx={{ bgcolor: "white" }}
+      style={{ height: "8vh", display: "flex", alignItems: "center" }}
+    >
+      <Grid item xs={4}>
+        <FormControl style={{ width: "13vw" }}>
+          <InputLabel id="state_select_label" style={{ fontWeight: "bold" }}>
+            State
+          </InputLabel>
+          <Select
+            labelId="state_select_label"
+            id="state_select"
+            value={dropLabel}
+            label="State"
+            onChange={handleChange}
+          >
+            {dropLabel === "Select State" && (
+              <MenuItem value={"Select State"}>Select State</MenuItem>
+            )}
+            <MenuItem value={"Arizona"}>Arizona</MenuItem>
+            <MenuItem value={"South Carolina"}>South Carolina</MenuItem>
+            <MenuItem value={"Texas"}>Texas</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="contained"
+          style={{ height: "5.5vh", width: "13vw", fontWeight: "bold" }}
+        >
+          This Is A Button
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="contained"
+          style={{ height: "5.5vh", width: "13vw", fontWeight: "bold" }}
+        >
+          This Is A Button 2
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
