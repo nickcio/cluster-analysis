b7a247dd49e585fb56a7465322d7b26c8c114d70
Brandy-Ma <brandy.ma@stonybrook.edu>
2023-10-02 22:44:36 -0400

    Co-authored-by: SebastianValdezSBU <SebastianValdezSBU@users.noreply.github.com> Co-authored-by: Nick Ciotoli <nickcio@users.noreply.github.com>

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,12 +4,7 @@ export const GlobalStoreContext = createContext({});
 
 export const GlobalStoreActionType = {
     SET_STATE: "SET_STATE",
-<<<<<<< HEAD
-    SET_ENSEMBLE: "SET_ENSEMBLE",
-    UPDATE_PATH: "UPDATE_PATH"
-=======
     SET_ENSEMBLE: "SET_ENSEMBLE"
->>>>>>> parent of 20490f6 (Changed some more)
 }
 
 function GlobalStoreContextProvider(props) {
@@ -17,12 +12,7 @@ function GlobalStoreContextProvider(props) {
     const [store, setStore] = useState({
         currentState: "",
         currentDistricts: "",
-<<<<<<< HEAD
-        currentEnsemble: "",
-        pathToCurrent: [{label:'Home', link:'/'}]
-=======
         currentEnsemble: ""
->>>>>>> parent of 20490f6 (Changed some more)
     });
 
     const storeReducer = (action) => {
@@ -35,29 +25,13 @@ function GlobalStoreContextProvider(props) {
                     ...store,
                     currentState: payload.state,
                     currentDistricts: payload.districts,
-<<<<<<< HEAD
-                    currentEnsemble:payload.currentEnsemble,
-                    pathToCurrent: payload.pathToCurrent
-=======
                     currentEnsemble:payload.currentEnsemble
->>>>>>> parent of 20490f6 (Changed some more)
                 });
             }
             case GlobalStoreActionType.SET_ENSEMBLE: {
                 return setStore({
                     ...store,
-<<<<<<< HEAD
-                    currentEnsemble: payload.ensemble,
-                    pathToCurrent: payload.pathToCurrent
-                });
-            }
-            case GlobalStoreActionType.UPDATE_PATH: {
-                return setStore({
-                    ...store,
-                    pathToCurrent: payload
-=======
                     currentEnsemble: payload.ensemble
->>>>>>> parent of 20490f6 (Changed some more)
                 });
             }
             default:
@@ -72,11 +46,7 @@ function GlobalStoreContextProvider(props) {
         payload_path.push({label:state.features[0].properties.NAME, link:"/state"});
         storeReducer({
             type: GlobalStoreActionType.SET_STATE,
-<<<<<<< HEAD
-            payload: {state:state,districts:districts,currentEnsemble:"", pathToCurrent:payload_path}
-=======
             payload: {state:state,districts:districts,currentEnsemble:""}
->>>>>>> parent of 20490f6 (Changed some more)
         });
 
     }
@@ -88,22 +58,9 @@ function GlobalStoreContextProvider(props) {
         payload_path.push({label:"Ensemble 1", link:"/state/ensemble"});
         storeReducer({
             type: GlobalStoreActionType.SET_ENSEMBLE,
-<<<<<<< HEAD
-            payload: {ensemble:ensemble, pathToCurrent:payload_path}
-        });
-
-    }
-
-    store.updatePath = async function(pathAddition) {
-        const payload = store.pathToCurrent;
-        payload.push(pathAddition);
-        storeReducer({
-            type: GlobalStoreActionType.UPDATE_PATH,
-            payload: payload
-=======
             payload: {ensemble:ensemble}
->>>>>>> parent of 20490f6 (Changed some more)
         });
+
     }
 
     return (
